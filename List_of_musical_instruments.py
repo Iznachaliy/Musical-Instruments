9b853aefa34c3f3b08bac4c3af9ffae2e549304b
AI Team <91843764+Iznachaliy@users.noreply.github.com>
2023-01-20 00:23:36 +0300

    Update List_of_musical_instruments.py

--- List_of_musical_instruments.py
+++ List_of_musical_instruments.py
@@ -1,1 +1,42 @@
+Chinese_musical_instruments = ["Se", "Guzheng", "Konghou", "Huluqin", "Huleiqin", "Pipa",
+                               "Liuqin", "Ruan", "Yueqin", "Qinqin", "Sanxian", "Duxianqin",
+                               "Huobosi", "Tembor", "Dutar", "Rawap", "Tianqin", "Qiben",
+                               "Wanqin", "Kongqin", "Qibue", "Saiding", "Dingbengba",
+                               "Palaung dingqin", "Sugudu", "Kaomuzi", "Huqin", "Erhu",
+                               "Zhonghu", "Gaohu", "Banhu", "Jinghu", "Jing erhu", "Erxian",
+                               "Tiqin", "Yehu", "Daguangxian", "Datong", "Kezaixian",
+                               "Liujiaoxian", "Tiexianzai", "Niujiaohu", "Huluhu", "Maguhu",
+                               "Tuhu", "Jiaohu", "Liuhu", "Sihu", "Sanhu", "Zhuihu", "Zhuiqin",
+                               "Leiqin", "Dihu", "Xiaodihu", "Zhongdihu", "Dadihu", "Dahu",
+                               "Cizhonghu", "Gehu", "Diyingehu", "Laruan", "Paqin", "Dapaqin",
+                               "Niutuiqin", "Matouqin", "Xiqin", "Shaoqin", "Yazheng",
+                               "Wenzhenqin", "Zhengni", "Qiaoqin", "Yangqin", "Zhu", "Niujinqin",
+                               "Wenqin", "Qingzhou cuoqin", "Dizi", "Bangdi", "Xiao", "Paixiao",
+                               "Chi", "Yue", "Xindi", "Dongdi", "Koudi", "Zhuxun", "Bawu",
+                               "Mangtong", "Mabu", "Guan", "Suona", "Zhu", "Yu", "Muyu",
+                               "Paiban", "Ban", "Zhuban", "Kuaiban", "Bangzi", "Nan bangzi",
+                               "Hebei bangzi", "Zhui bangzi", "Qin bangzi", "Bianqing",
+                               "Tezhong", "Bianzhong", "Fanxiang", "Nao", "Shangnao", "Bo",
+                               "Xiaobo", "Zhongbo", "Shuibo", "Dabo", "Jingbo", "Shenbo",
+                               "Luo", "Daluo", "Fengluo", "Xiaoluo", "Yueluo", "Jingluo",
+                               "Pingluo", "Kailuluo", "Yunluo", "Shimianluo", "Qing", "Daqing",
+                               "Pengling", "Dangzi", "Yingqing", "Yungzheng", "Chun", "Weichun",
+                               "Tonggu", "Laba", "Xun", "Fou", "Taodi", "Sheng", "Baosheng",
+                               "Yu", "Hulusi", "Hulusheng", "Fangsheng", "Dagu", "Huapengu",
+                               "Huzuo Dagu", "Huzuo Wujia Gu", "Jian'gu", "Bangu", "Biangu",
+                               "Paigu", "Tanggu", "Biqigu", "Diangu", "Huagu", "Yaogu", 
+                               "Taipinggu", "Zhangu", "Bajiaogu", "Yanggegu", "Gaogu", "Bofu",
+                               "Jiegu", "Tao", "Bolang Gu", "Linggu", "Gudi", "Hailuo",
+                               "Kouxian", "Yedi", "Shuijingdi", "Zutongqin"]
 
+def show_number_of_all_chinese_musical_instruments():
+    print (len(Chinese_musical_instruments))
+
+show_number_of_all_chinese_musical_instruments()
+
+                             
+                              
+                               
+                            
+                               
+                               
